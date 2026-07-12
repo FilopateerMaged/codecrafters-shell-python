c3b9446311ce1dfc5d4ad70d26b318377cdc01ea
FilopateerMaged <FilopateerMaged@users.noreply.github.com>
2026-07-12 02:55:15 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,13 +3,18 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
+    builtins = ["echo", "exit", "type"]
+
     while True:
         sys.stdout.write("$ ")
         user_input = sys.stdin.readline()
         if user_input.strip() == "exit":
             break
-        if user_input.startswith("echo "):
+        elif user_input.startswith("echo "):
             print(user_input.strip()[5:])
+        elif user_input.startswith("type "):
+            if user_input.strip()[5:] in builtins:
+                print(user_input.strip()[5:] + " is a shell builtin")
         else:
             print(user_input.strip() + ": command not found")
 
