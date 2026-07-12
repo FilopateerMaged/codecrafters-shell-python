f56e846b4b5d65728019d41ddced73089977d9d1
FilopateerMaged <FilopateerMaged@users.noreply.github.com>
2026-07-12 02:43:35 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
     user_input = sys.stdin.readline()
-    print(user_input.strip() + " command not found")
+    print(user_input.strip() + ": command not found")
 
     pass
 
