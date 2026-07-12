4d52d4ee5991594b2443a52bc4f15d58cbe30d69
FilopateerMaged <FilopateerMaged@users.noreply.github.com>
2026-07-12 02:42:35 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    user_input = sys.stdin.readline()
+    print(user_input.strip() + " is not a valid command.")
+
     pass
 
 
