46a0b6e16869ec7beb942dcf19162ebb5c8cc4d5
FilopateerMaged <FilopateerMaged@users.noreply.github.com>
2026-07-19 00:08:58 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -45,8 +45,8 @@ def run_executable(command):
     if not file_path:
         print(command_parts[0] + ": command not found")
     else:
-        print("Program was passed " + str(len(command_parts)) + " args (including program name).")
         os.execv(file_path, command_parts)
+        print("Program was passed " + str(len(command_parts)) + " args (including program name).")
 
 
 
